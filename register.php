13b2bf0ed3678598a650bbd3c37af39a5d82bb03
abdimk <abdisamk@gmail.com>
2024-05-25 14:24:47 +0300

    Fixing logins

--- register.php
+++ register.php
@@ -92,6 +92,7 @@ if($_SERVER['REQUEST_METHOD'] ==='POST'){
     <title>Sign up</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
+    <link rel="icon" href="/icon/icons8-l-64.png"type="image/x-icon">
 </head>
 
 <body>
